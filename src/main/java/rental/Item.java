1887b44ec0821dff7b156f193681f6e147f8f23c
seokangsik <kaing1125@gmail.com>
2020-07-15 19:09:13 +0900

    first commit

---
+++ src/main/java/rental/Item.java
@@ -0,0 +1,68 @@
+package rental;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Item_table")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String itemName;
+    private Integer amount;
+    private Integer price;
+
+    @PrePersist
+    public void onPrePersist(){
+        ItemCreated itemCreated = new ItemCreated();
+        BeanUtils.copyProperties(this, itemCreated);
+        itemCreated.publishAfterCommit();
+
+
+    }
+
+
+    @PostUpdate
+    public void onPostUpdate(){
+        ItemProduced itemProduced = new ItemProduced();
+        BeanUtils.copyProperties(this, itemProduced);
+        itemProduced.publishAfterCommit();
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+
+
+
+}
