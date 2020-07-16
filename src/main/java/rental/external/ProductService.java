026d29a53d977fa8db2455f81dec77bdd9b727ef
seokangsik <kaing1125@gmail.com>
2020-07-16 14:46:54 +0900

    Add files via upload

---
+++ src/main/java/rental/external/ProductService.java
@@ -0,0 +1,17 @@
+
+package rental.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@FeignClient(name="Product", url="http://Product:8080")
+public interface ProductService {
+
+    @RequestMapping(method= RequestMethod.GET, path="/products")
+    public void checkProduct(@RequestBody  Product product);
+
+}
